7a4326c88881fcb2e8c000011396af3838edb9f3
larissamoreira <larissamgusmao@gmail.com>
2019-01-18 16:44:09 -0300

    Add state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      'items': []
+    }
+  }
   componentDidMount() {
     this.getItems();
   }
@@ -9,11 +15,22 @@ class App extends Component {
   getItems() {
     fetch('http://127.0.0.1:8000/api/item/')
       .then(res => res.json())
-      .then(res => console.log(res))
+      .then(res => this.setState({ 'items': res }));
   }
 
   render() {
-    return null;
+    return (
+      <ul>
+        {this.state.items.map(function (item, index) {
+          return (
+            <div key={index}>
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+            </div>
+          )
+        })}
+      </ul>
+    )
   }
 }
 
