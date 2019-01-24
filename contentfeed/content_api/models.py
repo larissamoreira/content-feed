159763f9ad2751df38a5098068764b0ba5946772
Larissa Moreira <larissamgusmao@gmail.com>
2019-01-24 20:43:51 -0300

    Add Image django

--- contentfeed/content_api/models.py
+++ contentfeed/content_api/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Item(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=400)
+    image = models.ImageField(upload_to='images')
--- contentfeed/contentfeed/urls.py
+++ contentfeed/contentfeed/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
 from content_api.views import ItemViewSet
 
 router = routers.DefaultRouter()
@@ -9,4 +11,4 @@ router.register('item', ItemViewSet, base_name='test')
 urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
